b58458332fff134ef0e199e675c3358af0fba969
godlixe <allzllx19@gmail.com>
2022-12-09 19:50:34 +0700

    fix: stok remove auth

--- routes/stok_routes.go
+++ routes/stok_routes.go
@@ -2,14 +2,13 @@ package routes
 
 import (
 	"sinta-backend/controller"
-	"sinta-backend/middleware"
 	"sinta-backend/service"
 
 	"github.com/gin-gonic/gin"
 )
 
 func StokRoutes(router *gin.Engine, stokController controller.StokController, jwtService service.JWTService) {
-	stokRoutes := router.Group("/stok", middleware.Authenticate(jwtService))
+	stokRoutes := router.Group("/stok")
 	{
 		stokRoutes.GET("", stokController.GetStokByTokoID)
 		stokRoutes.POST("", stokController.InsertStok)
