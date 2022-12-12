5960b7ca62a34fcfaed746d402521389a4882d8e
godlixe <allzllx19@gmail.com>
2022-12-12 14:10:05 +0700

    feat: authenticate route for stok

--- routes/stok_routes.go
+++ routes/stok_routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"sinta-backend/controller"
+	"sinta-backend/middleware"
 	"sinta-backend/service"
 
 	"github.com/gin-gonic/gin"
 )
 
 func StokRoutes(router *gin.Engine, stokController controller.StokController, jwtService service.JWTService) {
-	stokRoutes := router.Group("/stok")
+	stokRoutes := router.Group("/stok", middleware.Authenticate(jwtService))
 	{
 		stokRoutes.GET("", stokController.GetStokByTokoID)
 		stokRoutes.POST("", stokController.InsertStok)
