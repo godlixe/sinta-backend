8d00f6400169c15733711dee3490443258a79823
godlixe <allzllx19@gmail.com>
2022-12-15 18:15:58 +0700

    fix: trx id

--- entity/detail_transaksi_entity.go
+++ entity/detail_transaksi_entity.go
@@ -4,7 +4,7 @@ type DetailTransaksi struct {
 	ID          uint64     `json:"id"`
 	ProdukID    uint64     `json:"produk_id" gorm:"foreignKey"`
 	Produk      *Produk    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"produk,omitempty"`
-	TransaksiID uint64     `json:"ajuan_id" gorm:"foreignKey"`
+	TransaksiID uint64     `json:"transaksi_id" gorm:"foreignKey"`
 	Transaksi   *Transaksi `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"transaksi,omitempty"`
 	TokoID      uint64     `json:"toko_id" gorm:"foreignKey"`
 	Toko        *Toko      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"toko,omitempty"`
