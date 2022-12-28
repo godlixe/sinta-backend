07d33867ec7917cf8ca5ff75d247a419decdc96e
godlixe <allzllx19@gmail.com>
2022-12-28 16:13:53 +0700

    debug query harian

--- repository/transaksi_repository.go
+++ repository/transaksi_repository.go
@@ -59,7 +59,7 @@ func (db *transaksiConnection) GetHarianTransaksiByTokoID(ctx context.Context, t
 	lastOfDay := firstOfDay.AddDate(0, 0, 1)
 
 	subQueryHarian := db.connection.Model(&entity.Transaksi{}).Select("id").Where(("toko_id = ?"), tokoID).Where("created_at >= ?", firstOfDay).Where("created_at <= ?", lastOfDay)
-	tx := db.connection.Model(&entity.DetailTransaksi{}).Where("transaksi_id IN (?)", subQueryHarian).Preload("Produk").Preload("Transaksi").Find(&daftarTransaksi)
+	tx := db.connection.Debug().Model(&entity.DetailTransaksi{}).Where("transaksi_id IN (?)", subQueryHarian).Preload("Produk").Preload("Transaksi").Find(&daftarTransaksi)
 	if tx.Error != nil {
 		return nil, tx.Error
 	}
